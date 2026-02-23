ff7e995a22afff33497cd4f76bb9b3af402a1835
Raul Campos <raulo_974@hotmail.com>
2026-02-23 17:27:38 -0500

    fix: actualizar apiUrl de produccion a Railway

--- preci-app/src/environments/environment.prod.ts
+++ preci-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.preci.pe',
+  apiUrl: 'https://preci-production.up.railway.app',
   mapboxToken: 'pk.eyJ1IjoicmFjaGNhbXBpdG9zIiwiYSI6ImNta2FxdDN6ZTI0YWwzY291Nm5ya2ZvbTcifQ.FdliekF2uQU0FC8jPUQFRA',
 };
